098e5f4f9e29021e237a84aadc5ea7c61e6408db
Markus Rezai <markus.rezai@gmail.com>
2019-11-23 12:50:17 +0100

    Added dummy file for trade changes.

---
+++ TallWideBalance/TradeChanges.sql
@@ -0,0 +1,6 @@
+-- TradeChanges
+-- Author: makak
+-- DateCreated: 2019-11-23 12:44:13
+--------------------------------------------------------------
+
+-- TODO: Changes to trade yields, especially transferring food
