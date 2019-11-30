fe1bf298efd7638c29b07800d8bd2bf6367fd83f
Markus Rezai <markus.rezai@gmail.com>
2019-11-30 14:57:02 +0100

    Corrections.

--- TallWideBalance/TradeChanges.sql
+++ TallWideBalance/TradeChanges.sql
@@ -34,7 +34,7 @@ VALUES
 -- CivicModifiers --
 --------------------
 
--- add trade route capacity increases
+-- trade route capacity increases from civics
 INSERT INTO District_TradeRouteYields (CivicType, ModifierId)
 VALUES
 	('CIVIC_GUILDS',		'CIVIC_GRANT_ONE_TRADE_ROUTE'),
@@ -44,9 +44,65 @@ VALUES
 -- TechnologyModifiers --
 -------------------------
 
--- add trade route capacity increases
+-- trade route capacity increases from technologies
 INSERT INTO District_TradeRouteYields (TechnologyType, ModifierId)
 VALUES
 	('TECH_CURRENCY',		'CIVIC_GRANT_ONE_TRADE_ROUTE'),
 	('TECH_BANKING',		'CIVIC_GRANT_ONE_TRADE_ROUTE'),
 	('TECH_ECONOMICS',		'CIVIC_GRANT_ONE_TRADE_ROUTE');
+
+---------------
+-- Modifiers --
+---------------
+
+-- modifiers for trade route increases from district and population
+INSERT INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId)
+VALUES
+	('MAK_COMMERCIAL_HUB_TRADE_ROUTE_CAPACITY',					'MODIFIER_PLAYER_ADJUST_TRADE_ROUTE_CAPACITY',	NULL),
+	('MAK_COMMERCIAL_HUB_POPULATION_15_TRADE_ROUTE_CAPACITY',	'MODIFIER_PLAYER_ADJUST_TRADE_ROUTE_CAPACITY',	'MAK_REQUIRES_POPULATION_15'),
+	('MAK_COMMERCIAL_HUB_POPULATION_30_TRADE_ROUTE_CAPACITY',	'MODIFIER_PLAYER_ADJUST_TRADE_ROUTE_CAPACITY',	'MAK_REQUIRES_POPULATION_30'),
+	('MAK_COMMERCIAL_HUB_POPULATION_40_TRADE_ROUTE_CAPACITY',	'MODIFIER_PLAYER_ADJUST_TRADE_ROUTE_CAPACITY',	'MAK_REQUIRES_POPULATION_40'),
+	('MAK_COMMERCIAL_HUB_POPULATION_50_TRADE_ROUTE_CAPACITY',	'MODIFIER_PLAYER_ADJUST_TRADE_ROUTE_CAPACITY',	'MAK_REQUIRES_POPULATION_50');
+
+-----------------------
+-- ModifierArguments --
+-----------------------
+
+-- modifier arguments for trade route increases from district and population
+INSERT INTO ModifierArguments (ModifierId, Name, Value)
+VALUES
+	('MAK_COMMERCIAL_HUB_TRADE_ROUTE_CAPACITY',					'Amount',	1),
+	('MAK_COMMERCIAL_HUB_POPULATION_15_TRADE_ROUTE_CAPACITY',	'Amount',	1),
+	('MAK_COMMERCIAL_HUB_POPULATION_30_TRADE_ROUTE_CAPACITY',	'Amount',	1),
+	('MAK_COMMERCIAL_HUB_POPULATION_40_TRADE_ROUTE_CAPACITY',	'Amount',	1),
+	('MAK_COMMERCIAL_HUB_POPULATION_50_TRADE_ROUTE_CAPACITY',	'Amount',	1);
+
+-----------------------
+-- DistrictModifiers --
+-----------------------
+
+-- add modifiers to districts
+INSERT INTO DistrictModifiers (DistrictType, ModifierId)
+VALUES
+	('DISTRICT_COMMERCIAL_HUB',		'MAK_COMMERCIAL_HUB_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_COMMERCIAL_HUB',		'MAK_COMMERCIAL_HUB_POPULATION_15_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_COMMERCIAL_HUB',		'MAK_COMMERCIAL_HUB_POPULATION_30_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_COMMERCIAL_HUB',		'MAK_COMMERCIAL_HUB_POPULATION_40_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_COMMERCIAL_HUB',		'MAK_COMMERCIAL_HUB_POPULATION_50_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_SUGUBA',				'MAK_COMMERCIAL_HUB_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_SUGUBA',				'MAK_COMMERCIAL_HUB_POPULATION_15_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_SUGUBA',				'MAK_COMMERCIAL_HUB_POPULATION_30_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_SUGUBA',				'MAK_COMMERCIAL_HUB_POPULATION_40_TRADE_ROUTE_CAPACITY'),
+	('DISTRICT_SUGUBA',				'MAK_COMMERCIAL_HUB_POPULATION_50_TRADE_ROUTE_CAPACITY');
+
+-----------------------
+-- BuildingModifiers --
+-----------------------
+
+-- remove trade route capacity from market
+DELETE FROM BuildingModifiers
+WHERE ModifierId = 'MARKET_TRADE_ROUTE_CAPACITY';
+
+-- remove trade route capacity from lighthouse
+DELETE FROM BuildingModifiers
+WHERE ModifierId = 'LIGHTHOUSE_TRADE_ROUTE_CAPACITY';
